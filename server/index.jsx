7186da75949f0654666f4f695836b4387480c2c2
mamun-hosen <hossainmamun035@gmail.com>
2020-04-18 15:23:44 +0600

    Added API routes and Mock data

--- server/index.jsx
+++ server/index.jsx
@@ -2,9 +2,39 @@ import express from "express";
 import yields from "express-yields";
 import fs from "fs-extra";
 import webpack from "webpack";
+import { argv } from "optimist";
+import { get } from "request-promise";
+import { questions, question } from "../data/api-real-url";
+import { delay } from "redux-saga";
 
 const port = process.env.PORT || 3000;
 const app = express();
+
+const useLiveData = argv.useLiveData === "true";
+
+function* getQuestions() {
+  let data;
+  if (useLiveData) {
+    data = yield get(questions, { gzip: true });
+  } else {
+    data = yield fs.readFile("./data/mock-questions.json", "utf-8");
+  }
+  return JSON.parse(data);
+}
+function* getQuestion(question_id) {
+  let data;
+  if (useLiveData) {
+    data = yield get(question(question_id), { gzip: true, json: true });
+  } else {
+    const questions = yield getQuestions();
+    const question = questions.items.find(
+      (question) => question.question_id == question_id
+    );
+    question.body = `Mock question body: ${question_id}`;
+    data = { items: [question] };
+  }
+  return data;
+}
 
 if (process.env.NODE_ENV === "development") {
   const config = require("../webpack.config.dev.babel").default;
@@ -18,6 +48,18 @@ if (process.env.NODE_ENV === "development") {
 
   app.use(require("webpack-hot-middleware")(compiler));
 }
+
+app.get("/api/questions", function* (req, res) {
+  const data = yield getQuestions();
+  yield delay(150);
+  res.json(data);
+});
+
+app.get("/api/questions/:id", function* (req, res) {
+  const data = yield getQuestion(req.params.id);
+  yield delay(150);
+  res.json(data);
+});
 
 app.get(["/"], function* (req, res) {
   let index = yield fs.readFile("./public/index.html", "utf-8");
