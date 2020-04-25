34f75438d052eedb852c348dc98aaf339b3c70c7
mamun-hosen <hossainmamun035@gmail.com>
2020-04-25 14:51:55 +0600

    React Added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const AppDisplay = () => {
+  return (
+    <div>
+      <h1>Isomorphic React</h1>
+    </div>
+  );
+};
+
+export default AppDisplay;
